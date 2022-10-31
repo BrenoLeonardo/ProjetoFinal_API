03f0701c6e20c5e3a3df34ae0e2a775186b1a076
Breno Leonardo ferreira de Paula <breno.leonardof@hotmail.com>
2022-10-31 15:38:58 -0300

    Delete Serratec_API-RESTful_Trabalho-Final-main directory

--- Serratec_API-RESTful_Trabalho-Final-main/E-commerce/src/main/java/org/serratec/ecommerce/api/security/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package org.serratec.ecommerce.api.security;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-	Usuario findByEmail(String email);
-}
